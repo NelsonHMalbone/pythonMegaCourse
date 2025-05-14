3ab67372a2ea8782344b045e6c9b7c2831f9ef28
Nelson Malbone <nhmalbone@icloud.com>
2025-05-14 15:34:54 -0400

    05-14 Working on storing infomation from the user

--- module_1/day_1/section1_day1.py
+++ module_1/day_1/section1_day1.py
@@ -1,9 +1,14 @@
 # 05-10
 # getting user input
+# anything inside the () is known as the argument
+# print and input are functions
+
 print("Derail Vally Run Log Workbook")
-print("Category input")
 category_input = input("Enter the Category here: ")
 category_code = input("Enter Category code: ")
 item_delivering = input("Enter the item to be deliver: ")
 time_bonus_input = input('Enter time bonus: ')
 train_value_input = input("Enter train value: ")
+
+# storing user input in a list
+
