64961fbbd776af78d359664f8030bf3b55d535cb
Nelson Malbone <nhmalbone@icloud.com>
2025-05-17 04:00:06 -0400

    05-17 appending each input to the list

--- app1/main.py
+++ app1/main.py
@@ -4,11 +4,27 @@ category_list = []
 
 while True:
     category_input = input("Enter the Category here: ")
-    category_code = input("Enter Category code: ")
-    item_delivering = input("Enter the item to be deliver: ")
-    time_bonus_input = input("Enter time bonus: ")
-    train_value_input = input("Enter train value: ")
-    train_length_input = input("Enter train length: ")
-    train_mass_input = input("Enter train mass: ")
-    license_requirements = input("Enter licenses require: ")
+    category_list.append(category_input)
 
+    category_code = input("Enter Category code: ")
+    category_list.append(category_code)
+
+    item_delivering = input("Enter the item to be deliver: ")
+    category_list.append(item_delivering)
+
+    time_bonus_input = input("Enter time bonus: ")
+    category_list.append(time_bonus_input)
+
+    train_value_input = input("Enter train value: ")
+    category_list.append(train_value_input)
+
+    train_length_input = input("Enter train length: ")
+    category_list.append(train_value_input)
+
+    train_mass_input = input("Enter train mass: ")
+    category_list.append(train_mass_input)
+
+    license_requirements = input("Enter licenses require: ")
+    category_list.append(license_requirements)
+
+    print(category_list)
