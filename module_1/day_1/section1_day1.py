042c73bb11bb36d3a5110587ac88e7cb10f9b88f
Nelson Malbone <nhmalbone@icloud.com>
2025-05-17 03:07:21 -0400

    05-17 fixed the '' to "" on line 10 added a print for category_list

--- module_1/day_1/section1_day1.py
+++ module_1/day_1/section1_day1.py
@@ -7,7 +7,7 @@ print("Derail Vally Run Log Workbook")
 category_input = input("Enter the Category here: ")
 category_code = input("Enter Category code: ")
 item_delivering = input("Enter the item to be deliver: ")
-time_bonus_input = input('Enter time bonus: ')
+time_bonus_input = input("Enter time bonus: ")
 train_value_input = input("Enter train value: ")
 train_length_input = input("Enter train length: ")
 train_mass_input = input("Enter train mass: ")
@@ -20,3 +20,4 @@ category_list = [category_input,
                  time_bonus_input,
                  train_value_input,
                  train_length_input]
+print(category_list)
