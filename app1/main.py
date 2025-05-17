3689374e1917b91857e56a00e2d67d4b1ed943c1
Nelson Malbone <nhmalbone@icloud.com>
2025-05-17 03:43:35 -0400

    05-17 added a while loop so when the user has enter all the infomation it will repeat and ask user for more

--- app1/main.py
+++ app1/main.py
@@ -1,18 +1,19 @@
 print("Derail Vally Run Log Workbook")
-category_input = input("Enter the Category here: ")
-category_code = input("Enter Category code: ")
-item_delivering = input("Enter the item to be deliver: ")
-time_bonus_input = input("Enter time bonus: ")
-train_value_input = input("Enter train value: ")
-train_length_input = input("Enter train length: ")
-train_mass_input = input("Enter train mass: ")
-license_requirements = input("Enter licenses require: ")
+while True:
+    category_input = input("Enter the Category here: ")
+    category_code = input("Enter Category code: ")
+    item_delivering = input("Enter the item to be deliver: ")
+    time_bonus_input = input("Enter time bonus: ")
+    train_value_input = input("Enter train value: ")
+    train_length_input = input("Enter train length: ")
+    train_mass_input = input("Enter train mass: ")
+    license_requirements = input("Enter licenses require: ")
 
 # storing user input in a list
-category_list = [category_input,
+    category_list = [category_input,
                  category_code,
                  item_delivering,
                  time_bonus_input,
                  train_value_input,
                  train_length_input]
-print(category_list)
+    print(category_list)
